ed3ce22db5513065428992a501397398a231ff85
saed <samersame404@gmail.com>
2022-07-10 22:00:21 +0300

    doc epc

--- app/src/main/java/com/hopeland/pda/example/SAED/Models/EpcModel.java
+++ app/src/main/java/com/hopeland/pda/example/SAED/Models/EpcModel.java
@@ -4,8 +4,21 @@ import android.util.Pair;
 
 import java.util.ArrayList;
 
+/**
+ * model to carrying data from EPC<br>
+ * <p> creating by saed </p>
+ */
 public class EpcModel {
+
+    /**
+     * paring to set data <br>
+     * first : for epc or tid data;<br>
+     * second : for item count;
+     */
     public Pair<String, Long> data;
+    /**
+     * to checking if data sent to server or not
+     */
     public boolean isSent;
 
     public void setData(Pair<String, Long> data) {
